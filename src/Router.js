af33f0b2ddac27c8388d623463378d3f1c7e18c2
gabin <lestratgabin@gmail.com>
2022-10-10 09:40:38 +0200

    Router et page terminé

---
+++ src/Router.js
@@ -0,0 +1,24 @@
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Error from "./pages/Error";
+import Logement from "./pages/Logement";
+
+const Routeur = () => {
+    return (
+        <Router>
+            <Routes>
+                <Route exact path="/" element={<Home />}>
+                </Route>
+                <Route exact path="/about" element={<About />}>
+                </Route>
+                <Route exact path="*" element={<Error />}>
+                </Route>
+                <Route exact path="/logement" element={<Logement />}>
+                </Route>
+            </Routes>
+        </Router>
+    )
+}
+
+export default Routeur
